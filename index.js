aafe825b8c548bade6a7c074704615c736b1925e
Mara Grau <mngrau97@gmail.com>
2022-10-23 05:02:41 -0500

    never pushed before

---
+++ index.js
@@ -0,0 +1,54 @@
+console.log("Hello World")
+
+// 1: create a new funtion that takes event as a parameter
+
+function addMovie(event) {
+    event.preventDefault();
+
+    let inputField = document.querySelector("input");
+
+    const movie = document.createElement("li");
+
+    const movieTitle = document.createElement("span");
+    movieTitle.textContent = inputField.value
+    movieTitle.addEventListener("click", crossOffMovie);
+    movie.appendChild(movieTitle);
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.textContent = "X";
+    deleteBtn.addEventListener("click", deleteMovie);
+    movie.appendChild(deleteBtn);
+
+    document.querySelector("ul").appendChild(movie);
+
+    inputField.value = ""
+}
+
+document.querySelector("form").addEventListener("submit", addMovie);
+
+
+
+function deleteMovie(event) {
+    message.textContent = "Movie deleted!";
+    
+    event.target.parentNode.remove();
+}
+
+
+function crossOffMovie(event) {
+    event.target.classList.toggle("checked");
+
+    if (event.target.classList.contains("checked") === true) {
+        message.textContent = "Movie watched!";
+    } else {
+        message.textContent = "Movie added back!";
+    }
+}
+
+
+
+
+
+
+
+
